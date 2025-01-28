e296305ae84cbcbf7c56bdd1055293f7b5cfe01a
anzwkk <anam89737@gmail.com>
2025-01-29 01:46:28 +0300

    Added a basic structure for the to-do list

--- 13.1HW/Program.cs
+++ 13.1HW/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] tasks = new string[100];
+        bool[] isCompleted = new bool[100];
+        int taskCount = 0;
+
+        while (true) 
+        {
+            Console.WriteLine("Список справ");
+            Console.WriteLine("1. Додати справу");
+            Console.WriteLine("2. Вивести всі справи");
+            Console.WriteLine("3. Відмітити справу як виконану");
+            Console.WriteLine("4. Видалити справу");
+            Console.WriteLine("Оберіть опцію(1-4): ");
+        }
+    }
+    
+}
